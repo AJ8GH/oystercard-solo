83869188b6f7376349cb27abb5548ab720df71e2
Adam Jonas <adamjonas25@gmail.com>
2021-02-11 11:28:13 +0000

    define #top_up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,15 @@
 class Oystercard
   attr_reader :balance
-  
+
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    self.balance += 1
+  end
+
+  private
+
+  attr_writer :balance
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,5 +5,14 @@ describe Oystercard do
         expect(subject.balance).to be_zero
       end
     end
+
+    describe '#top_up' do
+      context 'with argument 1' do
+        it 'adds 1 to balance' do
+          subject.top_up(1)
+          expect(subject.balance).to be 1
+        end
+      end
+    end
   end
 end
