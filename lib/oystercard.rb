8a18e19046ed4879ae682b83b72b4349286afcd6
Adam Jonas <adamjonas25@gmail.com>
2021-02-11 11:56:19 +0000

    define touch in and touch out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,11 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   MAX_BALANCE = 90
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -12,11 +13,27 @@ class Oystercard
     self.balance += 1
   end
 
+  def in_journey?
+    in_journey
+  end
+
+  def touch_in
+    self.in_journey = true
+  end
+
+  def touch_out
+    self.in_journey = false
+  end
+
   private
 
-  attr_writer :balance
+  attr_writer :balance, :in_journey
 
   def over_limit?(amount)
     balance + amount > MAX_BALANCE
+  end
+
+  def deduct(amount)
+    self.balance -= amount
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,25 +1,56 @@
 describe Oystercard do
   describe '#balance' do
     context 'when initialized' do
-      it 'is zero' do
-        expect(subject.balance).to be_zero
+      subject { described_class.new.balance }
+      it { is_expected.to be_zero }
+    end
+  end
+
+  describe '#top_up' do
+    context 'when topping up 1' do
+      it 'adds 1 to balance' do
+        subject.top_up(1)
+        expect(subject.balance).to be 1
       end
     end
 
-    describe '#top_up' do
-      context 'when topping up 1' do
-        it 'adds 1 to balance' do
-          subject.top_up(1)
-          expect(subject.balance).to be 1
-        end
+    context 'when balance goes over limit' do
+      it 'raises error' do
+        expect {
+          subject.top_up(described_class::MAX_BALANCE + 1)
+        }.to raise_error MaxBalanceError
       end
+    end
+  end
 
-      context 'when balance goes over limit' do
-        it 'raises error' do
-          expect {
-            subject.top_up(described_class::MAX_BALANCE + 1)
-          }.to raise_error MaxBalanceError
-        end
+  describe '#in_journey' do
+    context 'when initialized' do
+      it { is_expected.not_to be_in_journey }
+    end
+  end
+
+  describe '#touch_in' do
+    context 'after touching in' do
+      before { subject.touch_in }
+      it { is_expected.to be_in_journey }
+    end
+  end
+
+  describe '#touch_out' do
+    context 'after touching in then touching out' do
+      before { subject.touch_in; subject.touch_out }
+      it { is_expected.not_to be_in_journey }
+    end
+  end
+
+  # private
+  describe '#deduct' do
+    before { subject.top_up(1) }
+
+    context 'when deducting 1' do
+      it 'removes 1 from balance' do
+        subject.send(:deduct, 1)
+        expect(subject.balance).to be_zero
       end
     end
   end
